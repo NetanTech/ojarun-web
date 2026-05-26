e1b522dc2c2508edbaecafca9de38228d075604f
Code-minner <144187185+Code-minner@users.noreply.github.com>
2026-05-26 12:01:48 +0100

    terms update

--- src/app/(Terms)/terms-and-conditions/components/details.tsx
+++ src/app/(Terms)/terms-and-conditions/components/details.tsx
@@ -406,11 +406,8 @@ const sections: Section[] = [
 export default function Detail() {
   return (
     <section className="relative w-full bg-white px-4 sm:px-8 py-20">
-      <motion.div
-        variants={stagger(0.08, 0.1)}
-        initial="hidden"
-        whileInView="show"
-        viewport={{ once: true, amount: 0.1 }}
+      <div
+     
         className="mx-auto max-w-[900px]"
       >
         {sections.map((section) => (
@@ -475,7 +472,7 @@ export default function Detail() {
             })}
           </motion.article>
         ))}
-      </motion.div>
+      </div>
     </section>
   );
 }
